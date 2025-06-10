004318ad8ab0366d21af2fdb8c46f3c789ed4f10
Douglas Panhota <douglaspands@gmail.com>
2025-06-10 09:51:37 -0300

    fix: purchase balance (#15)

--- internal/service/security.go
+++ internal/service/security.go
@@ -2,32 +2,72 @@ package service
 
 import (
 	"math"
+	"sort"
 	"trader/internal/resource"
 )
 
 func MakePurchaseBalance(securities []*resource.Security, amountInvested float64) *resource.PurchaseBalance {
-	stockCount := len(securities)
-	stockValue := amountInvested / float64(stockCount)
+	securityCount := len(securities)
+	securityValue := amountInvested / float64(securityCount)
 	remainingBalance := amountInvested
 	priceMin := math.MaxFloat64
+	priceMax := float64(0)
 	securitiesPurchase := make([]*resource.SecurityPurchase, 0)
-	for _, stock := range securities {
-		if stock.Price < priceMin {
-			priceMin = stock.Price
+	securitiesPurchaseSort := make([]*resource.SecurityPurchase, 0)
+	securitiesExpensive := make([]*resource.Security, 0)
+	countBalance := 0
+	for _, security := range securities {
+		if security.Price < priceMin {
+			priceMin = security.Price
 		}
-		securityPurchase := &resource.SecurityPurchase{
-			Security: stock,
-			Count:    int(stockValue / stock.Price),
+		if security.Price > priceMax {
+			priceMax = security.Price
 		}
-		securitiesPurchase = append(securitiesPurchase, securityPurchase)
-		remainingBalance = remainingBalance - securityPurchase.TotalAmount()
 	}
+	if amountInvested < priceMin {
+		return &resource.PurchaseBalance{SecuritiesBalance: securitiesPurchase, AmountInvested: amountInvested}
+	}
+	if securityValue < priceMax {
+		countBalance = -1
+	}
+	for _, security := range securities {
+		count := int(securityValue / security.Price)
+		if count < 1 {
+			securitiesExpensive = append(securitiesExpensive, security)
+		} else {
+			if (count + countBalance) > 0 {
+				count = count + countBalance
+			}
+			securityPurchase := &resource.SecurityPurchase{
+				Security: security,
+				Count:    count,
+			}
+			securitiesPurchase = append(securitiesPurchase, securityPurchase)
+			securitiesPurchaseSort = append(securitiesPurchaseSort, securityPurchase)
+			remainingBalance = remainingBalance - securityPurchase.TotalAmount()
+		}
+	}
+	for _, security := range securitiesExpensive {
+		if remainingBalance >= security.Price {
+			count := 1
+			securityPurchase := &resource.SecurityPurchase{
+				Security: security,
+				Count:    count,
+			}
+			securitiesPurchase = append(securitiesPurchase, securityPurchase)
+			securitiesPurchaseSort = append(securitiesPurchaseSort, securityPurchase)
+			remainingBalance = remainingBalance - securityPurchase.TotalAmount()
+		}
+	}
+	sort.Slice(securitiesPurchaseSort, func(i, j int) bool {
+		return securitiesPurchaseSort[i].Security.Price < securitiesPurchaseSort[j].Security.Price
+	})
 	for {
 		if remainingBalance >= priceMin {
-			for i := range stockCount {
-				if remainingBalance >= securitiesPurchase[i].Security.Price {
-					securitiesPurchase[i].Count += 1
-					remainingBalance = remainingBalance - securitiesPurchase[i].Security.Price
+			for i := range securityCount {
+				if remainingBalance >= securitiesPurchaseSort[i].Security.Price {
+					securitiesPurchaseSort[i].Count += 1
+					remainingBalance = remainingBalance - securitiesPurchaseSort[i].Security.Price
 				}
 			}
 		} else {
