fa8d74e32e31f5b0821639a3338f2919ad26219f
Douglas Panhota <douglaspands@gmail.com>
2026-01-17 20:14:09 -0300

    tools.table: 100% test coverage

--- internal/tools/table_test.go
+++ internal/tools/table_test.go
@@ -2,25 +2,74 @@ package tools_test
 
 import (
 	"testing"
+	"time"
 	"trader/internal/tools"
 )
 
-func TestTableRowValue(t *testing.T) {
+func TestTableRowValueFloatFormat(t *testing.T) {
+
+	// EXPECTED
+	expected := "10.50"
+
 	// GIVEN
-	value := map[string]interface{}{
-		"price": 10.50,
-		"name":  "Test",
+	price := 10.50
+
+	// THEN
+	result := tools.TableRowValue(price)
+
+	// WHEN
+	if result.(string) != expected {
+		t.Errorf(`expected at %s and received at %s`, expected, result.(string))
 	}
+}
+
+func TestTableRowValueTimeFormat(t *testing.T) {
+
+	// EXPECTED
+	expected := "2022-01-01 00:00:00"
+
+	// GIVEN
+	date := time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	// THEN
+	result := tools.TableRowValue(date)
+
+	// WHEN
+	if result.(string) != expected {
+		t.Errorf(`expected at %s and received at %s`, expected, result.(string))
+	}
+}
+
+func TestTableRowValueStringFormat(t *testing.T) {
+
+	// EXPECTED
+	expected := "Test"
+
+	// GIVEN
+	value := "Test"
 
 	// THEN
 	result := tools.TableRowValue(value)
 
 	// WHEN
-	if result.(map[string]interface{})["price"] != 10.50 {
-		t.Errorf(`expected at %f and received at %f`, 10.50, result.(map[string]interface{})["price"])
+	if result.(string) != expected {
+		t.Errorf(`expected at %s and received at %s`, expected, result.(string))
 	}
+}
 
-	if result.(map[string]interface{})["name"] != "Test" {
-		t.Errorf(`expected at "%s" and received at %s`, "Test", result.(map[string]interface{})["name"])
+func TestTableRowValueIntFormat(t *testing.T) {
+
+	// EXPECTED
+	expected := 10
+
+	// GIVEN
+	value := 10
+
+	// THEN
+	result := tools.TableRowValue(value)
+
+	// WHEN
+	if result.(int) != expected {
+		t.Errorf(`expected at %d and received at %d`, expected, result.(int))
 	}
 }
